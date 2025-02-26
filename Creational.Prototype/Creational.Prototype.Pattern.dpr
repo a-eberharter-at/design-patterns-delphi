572bb2443980fbfc2a22f3bb62df9f5ca22864c0
a-eberharter-at <aeberharter87@gmail.com>
2025-02-26 13:15:44 +0100

    Adds Prototype Pattern example.

--- Creational.Prototype/Creational.Prototype.Pattern.dpr
+++ Creational.Prototype/Creational.Prototype.Pattern.dpr
@@ -6,12 +6,30 @@ uses
   SysUtils,
   Pattern in 'Pattern.pas';
 
+var
+  prototypeA, clonedA: IPrototype;
+  prototypeB, clonedB: IPrototype;
+
 begin
-  ReportMemoryLeaksOnShutDown := DebugHook <> 0;
   try
-    { TODO -oUser -cConsole Main : Insert code here }
+    // Create initial prototypes
+    prototypeA := TConcretePrototypeA.Create;
+    prototypeB := TConcretePrototypeB.Create;
+
+    // Clone the prototypes
+    clonedA := prototypeA.Clone;
+    clonedB := prototypeB.Clone;
+
+    // Output results
+    Writeln(prototypeA.GetDescription);
+    Writeln(clonedA.GetDescription);
+    Writeln(prototypeB.GetDescription);
+    Writeln(clonedB.GetDescription);
+
+    ReadLn;
   except
-    on E:Exception do
-      Writeln(E.Classname, ': ', E.Message);
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
   end;
 end.
+
--- Creational.Prototype/Pattern.pas
+++ Creational.Prototype/Pattern.pas
@@ -2,6 +2,49 @@ unit Pattern;
 
 interface
 
+type
+  IPrototype = interface
+  ['{3D94E6F0-EC5C-4D3D-B2D7-33475B2A9C98}']
+    function Clone: IPrototype;
+    function GetDescription: string;
+  end;
+
+  TConcretePrototypeA = class(TInterfacedObject, IPrototype)
+  public
+    function Clone: IPrototype;
+    function GetDescription: string;
+  end;
+
+  TConcretePrototypeB = class(TInterfacedObject, IPrototype)
+  public
+    function Clone: IPrototype;
+    function GetDescription: string;
+  end;
+
 implementation
+
+{ TConcretePrototypeA }
+
+function TConcretePrototypeA.Clone: IPrototype;
+begin
+  Result := TConcretePrototypeA.Create;
+end;
+
+function TConcretePrototypeA.GetDescription: string;
+begin
+  Result := 'Prototype A';
+end;
+
+{ TConcretePrototypeB }
+
+function TConcretePrototypeB.Clone: IPrototype;
+begin
+  Result := TConcretePrototypeB.Create;
+end;
+
+function TConcretePrototypeB.GetDescription: string;
+begin
+  Result := 'Prototype B';
+end;
 
 end.
